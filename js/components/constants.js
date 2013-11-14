d2350e62ac18a62db5be4ada57c2aa20c7bf8c6d
Steve Nguyen <st.nguyen09@gmail.com>
2013-11-13 23:29:38 -0500

    Replaced local imports with remote urls

--- js/components/constants.js
+++ js/components/constants.js
@@ -1,8 +1,8 @@
 var constants = {
-	D3_IMPORT: "<script type=\"text/javascript\" src=\"/js/d3/d3.v3.js\"></script>",
-	JQUERY_IMPORT: "<script src=\"http://code.jquery.com/jquery-1.9.1.js\"></script>",
-	JQUERY_UI_IMPORT: "<script src=\"http://code.jquery.com/ui/1.10.3/jquery-ui.js\"></script>",
+	D3_IMPORT: "<script src=\"http://d3js.org/d3.v3.min.js\"></script>",
+	JQUERY_IMPORT: "<script src=\"https://code.jquery.com/jquery-1.9.1.js\"></script>",
+	JQUERY_UI_IMPORT: "<script src=\"https://code.jquery.com/ui/1.10.3/jquery-ui.js\"></script>",
 	COMMON_STYLE_IMPORT: "<link rel=\"stylesheet\" type=\"text/css\" href=\"style/style.css\">",
-	JQUERY_UI_STYLE_IMPORT: "<script src=\"http://code.jquery.com/ui/1.10.3/jquery-ui.js\"></script>",
+	JQUERY_UI_STYLE_IMPORT: "<script src=\"https://code.jquery.com/ui/1.10.3/jquery-ui.js\"></script>",
 	MAP_PREVIEW_ID: "map_preview"
 };
