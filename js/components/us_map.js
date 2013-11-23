66109eeedbe0074f028e56a62eb1ba346e3d48e3
Steve Nguyen <st.nguyen09@gmail.com>
2013-11-22 19:42:55 -0500

    Removed unneeded comments

--- js/components/us_map.js
+++ js/components/us_map.js
@@ -253,16 +253,3 @@ var us_map = {
 		state.widgets.us_map.highlightingEnabled = enable;
 	}
 };
-
-/*
-This is the general form of the unique namespace. To call a function in this, we call 
-us_map.functioncall(). To interact with the frame, we get it by calling $("#" + constants.MAP_PREVIEW_ID).contents();. 
-The svg element is created here when generating the map. To get it we use $("#" + constants.MAP_PREVIEW_ID).contents().find('body').find('svg');. 
-That returns the svg element and you can do whatever you want with it like appending new things.
-The benefit of doing things this way is that we decouple the different components from the main index page. 
-The only things tying these components to the page are a script import at the top of the index page and the actual function calls. 
-This makes it easy to add and remove components. We can even generate the html from these js files so they actually render a button 
-with the appropriate onclick events. Then we can pretty much render almost the entire index page in a script.
-
-I understand that this isn't exactly a "plugin" system in its truest form, but does allow for easy the easy addition and removal of components.
-*/
