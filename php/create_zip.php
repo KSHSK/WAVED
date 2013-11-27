48fce6e189c195cdb1eac0248a3e8ef0db335467
Sean Bluestein <sb679@cs.drexel.edu>
2013-11-26 23:25:09 -0500

    Export zip containing HTML and data files.

---
+++ php/create_zip.php
@@ -0,0 +1,57 @@
+<?php
+
+// Get the name of the demo.
+$demo = $_POST["demo"];
+if ($demo == "") {
+   echo "App name cannot be blank.";   
+   return;
+}
+
+// Get the HTML
+$html = $_POST["html"];
+if ($html == "") {
+   echo "HTML cannot be blank.";
+   return;
+}
+
+// Get the data files.
+$data_files = $_POST["data_files"];
+if ($data_files == null) {
+   // Default to no data files.
+   $data_files = array();
+}
+
+// Go back to the main directory.
+chdir("../");
+
+$folder = $demo . (end(str_split($demo)) == "/" ? "" : "/");
+$zipName = $demo . '.zip';
+
+$generated_location = "generated_files/";
+
+$zipPath = $generated_location . $zipName;
+
+// Create the zipfile in the $generated_location directory.
+// The zip folder will contain the a root directory named $demo, containing the generated HTML file, and all data files.
+$zip = new ZipArchive;
+$zip->open($zipPath, ZipArchive::CREATE | ZIPARCHIVE::OVERWRITE);
+$zip->addEmptyDir($folder);
+
+// Add the HTML file.
+$zip->addFromString($folder . "index.html", $html);
+
+// Add all of the data files.
+foreach ($data_files as $file) {
+   $zip_path = $folder . $file;
+   $zip->addFile($file, $zip_path);
+}
+
+$zip->close();
+
+if (!file_exists($zipPath)) {
+   echo "Failed to create zip file.";
+   return;
+}
+
+echo "success";
+?>
