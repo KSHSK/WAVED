06790a4900a9e82e15a8583e33a6f08464f66a32
seanblue <5884067+seanblue@users.noreply.github.com>
2013-11-27 13:53:19 -0500

    Upload a new file to the data folder and add it to the dropdown.

---
+++ php/upload_data_file.php
@@ -0,0 +1,40 @@
+<?php
+
+$file = $_FILES["file"];
+
+// Setup return object.
+$returnValue = array("errorMessage" => "", "success" => true, "filePath" => "");
+
+if ($file == null) {
+   $returnValue["success"] = false;
+   $returnValue["errorMessage"] = "File is missing.";
+   echo json_encode($returnValue);
+   return;
+}
+
+// Go back to the main directory.
+chdir("../");
+
+$fileName = $file["name"];
+$filePath = "data/" . $fileName;
+$tempName = $file["tmp_name"];
+
+if (file_exists($filePath)) {
+   $returnValue["success"] = false;
+   $returnValue["errorMessage"] = "This file already exists.";
+   echo json_encode($returnValue);
+   return;
+}
+
+$success = move_uploaded_file($tempName, $filePath);
+if (!$success) {
+   $returnValue["success"] = false;
+   $returnValue["errorMessage"] = "Failed to create file.";
+   echo json_encode($returnValue);
+   return;
+}
+
+$returnValue["filePath"] = $filePath;
+echo json_encode($returnValue);
+
+?>
