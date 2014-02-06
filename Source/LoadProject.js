b46737b7696add2f7f5d7953c54fc43477c5d83f
Kristian Calhoun <kristian.calhoun@gmail.com>
2014-02-06 01:32:03 -0500

    Refactor Welcome/New Project code.

---
+++ Source/LoadProject.js
@@ -0,0 +1,17 @@
+/**
+ * A module for loading an existing project.
+ */
+define(['jquery'], function($) {
+    var LoadProjectModule = {
+        tryToLoadExistingProject: function() {
+            // TODO Implement me.
+            
+            var deferred = $.Deferred();
+            
+            // Remove this reject when implementing.
+            deferred.reject();
+            
+            return deferred.promise();
+        }
+    };
+});
