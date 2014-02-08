fd343d1e834f688c8fa31399255d8b957efc2aa5
Steve Nguyen <st.nguyen09@gmail.com>
2014-02-08 16:34:51 -0500

    Merge branch 'master' into GA-Panel-UI
    Conflicts:
    Source/WAVED.js
    Source/main.js

--- Source/main.js
+++ Source/main.js
@@ -1,114 +1,15 @@
-require(["WAVED"], function(WAVED) {
-
-    // UI Setup
-    $(document).ready(function() {
-        setupUI();
-        displayPage();
-        WAVED.start();
-    });
+require.config({
+    paths: {
+        angular: '../ThirdParty/angularjs/angular.min',
+        jquery: 'require-jquery'
+    },
     
-    function setupUI() {
-        $('#new-button').button();
-        $('#undo-button').button();
-        $('#redo-button').button();
-        $('#load-button').button();
-        $('#save-button').button();
-        $('#export-button').button();
-        $('#refresh-button').button();
-        $('#google-analytics-add-button').button();
-        $('#google-analytics-remove-button').button();
-        $('#google-analytics-clear-button').button();
-        
-        // Add/Edit/Remove Buttons.
-        $('#add-action-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-plus"
-            }
-        });
-
-        $('#edit-action-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-pencil"
-            }
-        });
-
-        $('#delete-action-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-trash"
-            }
-        });
-
-        $('#add-event-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-plus"
-            }
-        });
-
-        $('#edit-event-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-pencil"
-            }
-        });
-
-        $('#delete-event-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-trash"
-            }
-        });
-
-        $('#add-data-file-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-plus"
-            }
-        });
-
-        $('#delete-data-file-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-trash"
-            }
-        });
-
-        $('#add-data-subset-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-plus"
-            }
-        });
-
-        $('#edit-data-subset-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-pencil"
-            }
-        });
-
-        $('#delete-data-subset-button').button({
-            text:false,
-            icons: {
-                primary: "ui-icon-trash"
-            }
-        });
-        
-        // Setup accordion
-        $('#accordion').accordion({
-            animate: false
-        });
+    // AngularJS doesn't use define(), so we configure it as a module here.
+    shim: {
+        'angular' : {'exports' : 'angular'}
+    },
     
-        $('input').addClass('ui-corner-all');
-    }
+    // Load bootstrap.js to start the application.
+    deps: ['./bootstrap']
 
-    function displayPage() {
-        $('.hide-on-load').removeClass("hide-on-load");
-        
-        // Accordion must calculate size after it is visible to avoid sizing issues.
-        $('#accordion').accordion("refresh");
-    }
 });
