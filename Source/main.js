f7d34ecad5e76f572c54be822b086f6d96eb848f
hpinkos <pinkos.hannah@gmail.com>
2014-02-20 15:49:00 -0500

    cleanup

--- Source/main.js
+++ Source/main.js
@@ -1,9 +1,10 @@
 /*global require*/
 require.config({
     paths: {
-        jquery: '../ThirdParty/jquery-ui-1.10.4.custom/js/jquery-1.10.2',
-        jqueryUI: '../ThirdParty/jquery-ui-1.10.4.custom/js/jquery-ui-1.10.4.custom.min'
-        knockout: '../ThirdParty/knockout/knockout'    },
+        jquery: '../ThirdParty/jquery/js/jquery-1.10.2',
+        jqueryUI: '../ThirdParty/jquery/js/jquery-ui-1.10.4.custom.min',
+        knockout: '../ThirdParty/knockout/knockout'
+    },
 
     shim: {
         'jqueryUI': {
