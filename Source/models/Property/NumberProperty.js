0d1715c4ebbde1ae7c463d006ec73f139521c444
hpinkos <pinkos.hannah@gmail.com>
2014-02-23 18:34:55 -0500

    property inheritance

--- Source/models/Property/NumberProperty.js
+++ Source/models/Property/NumberProperty.js
@@ -12,8 +12,10 @@ define([
     ){
     'use strict';
     var NumberProperty = function(state) {
+        Property.call(this, state);
+
         state = defined(state) ? state : {};
-//        Property.call(this, state);
+
         this._value = state.value;
         if (defined(state.validValue)) {
             this.isValidValue = state.validValue;
@@ -22,14 +24,12 @@ define([
                 return true;
             };
         }
-        this._displayName = state.displayName;
         this._templateName = PropertyTemplateName.NUMBER;
 
         ko.track(this);
     };
 
-//    NumberProperty.prototype = new Property({displayName: ''});
-//    NumberProperty.prototype.constructor = NumberProperty;
+    NumberProperty.prototype = Object.create(Property.prototype);
 
     Object.defineProperties(NumberProperty.prototype, {
         value: {
@@ -40,16 +40,6 @@ define([
                 if (!isNaN(Number(value)) && this.isValidValue(value)) {
                     this._value = Number(value);
                 }
-            }
-        },
-        displayName: {
-            get: function() {
-                return this._displayName;
-            }
-        },
-        templateName : {
-            get: function() {
-                return this._templateName;
             }
         }
     });
