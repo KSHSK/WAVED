635214e56cde43bf765ddb112355b35d8b54d6a9
Steve Nguyen <st.nguyen09@gmail.com>
2014-02-25 12:04:29 -0500

    Created enums, moved existing to /Source/models/Constants, updated references

---
+++ Source/models/Constants/GlyphSizeSchemeType.js
@@ -0,0 +1,11 @@
+/*global define*/
+define([], function(){
+    'use strict';
+
+    var GlyphSizeSchemeType = {
+        CONSTANT_SIZE: 'constantGlyphSizeSchemeType',
+        SCALED_SIZE: 'scaledGlyphSizeSchemeType'
+    };
+
+    return GlyphSizeSchemeType;
+});
