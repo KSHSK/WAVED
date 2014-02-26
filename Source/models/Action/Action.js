1ffd8f770c418785ec9893087dbbe22cdee0fab8
Steve Nguyen <st.nguyen09@gmail.com>
2014-02-25 22:57:39 -0500

    Action skeleton classes

---
+++ Source/models/Action/Action.js
@@ -0,0 +1,37 @@
+/*global define*/
+define(['knockout',
+        'util/defined'
+    ],function(
+        ko,
+        defined
+    ){
+    'use strict';
+
+    var Action = function(state) {
+        state = defined(state) ? state : {};
+
+        // TODO: Validation, etc
+        this._name = state.displayName; // String
+        this._target = state.target; // TODO: Get the actual target
+        this._applyAutmatically = state.applyAutomatically; // Boolean
+
+       var apply = function() {
+
+       };
+
+        ko.track(this);
+    };
+
+    Object.defineProperties(Action.prototype, {
+        name: {
+            get: function() {
+                return this._name;
+            },
+            set: function(value) {
+                this._name = value;
+            }
+        }
+    });
+
+    return Action;
+});
