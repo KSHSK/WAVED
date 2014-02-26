143f2fe8ba9fca00e1d2c96339f445d8d953006b
Steve Nguyen <st.nguyen09@gmail.com>
2014-02-25 22:01:31 -0500

    DeleteData, DeleteProject, ExportProject, LoadData, SaveProject class skeletons

---
+++ Source/modules/ExportProject.js
@@ -0,0 +1,29 @@
+/*global define*/
+/**
+ * A module for exporting the current project.
+ */
+define([
+        '../WavedViewModel',
+        'jquery'
+    ], function(
+        WAVEDViewModel,
+        $) {
+    'use strict';
+
+    var ExportProject = {
+
+        tryToExportProject: function(viewModel){
+
+        },
+
+        openExportProjectDialog: function(viewModel){
+
+        },
+
+        exportProject: function(projectName, viewModel){
+
+        }
+    };
+
+    return ExportProject;
+});
---
+++ Source/modules/SaveProject.js
@@ -0,0 +1,29 @@
+/*global define*/
+/**
+ * A module for saving a project.
+ */
+define([
+        '../WavedViewModel',
+        'jquery'
+    ], function(
+        WAVEDViewModel,
+        $) {
+    'use strict';
+
+    var SaveProject = {
+
+        tryToSaveProject: function(viewModel){
+
+        },
+
+        openSaveProjectDialog: function(viewModel){
+
+        },
+
+        saveProject: function(projectName, viewModel){
+
+        }
+    };
+
+    return SaveProject;
+});
