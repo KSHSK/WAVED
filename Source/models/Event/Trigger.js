abdf7dc32a69f2b2baead4564c7b4e59ed8aa092
Steve Nguyen <st.nguyen09@gmail.com>
2014-02-25 23:07:14 -0500

    Event and Trigger skeleton classes

---
+++ Source/models/Event/Trigger.js
@@ -0,0 +1,31 @@
+/*global define*/
+define(['knockout',
+        'util/defined'
+    ],function(
+        ko,
+        defined
+    ){
+    'use strict';
+
+    var Trigger = function(state) {
+        state = defined(state) ? state : {};
+
+        // TODO: Validation, etc
+        this._name = state.name; // String
+
+        ko.track(this);
+    };
+
+    Object.defineProperties(Trigger.prototype, {
+        name: {
+            get: function() {
+                return this._name;
+            },
+            set: function(value) {
+                this._name = value;
+            }
+        }
+    });
+
+    return Trigger;
+});
