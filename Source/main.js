512dd26b2d4a37b3444dca0e24d9f513fb60826e
Steve Nguyen <st.nguyen09@gmail.com>
2014-03-02 14:37:02 -0500

    Add koExternalTemplateEngine files, initial template files

--- Source/main.js
+++ Source/main.js
@@ -3,13 +3,17 @@ require.config({
     paths: {
         jquery: '../ThirdParty/jquery/js/jquery-1.10.2',
         jqueryUI: '../ThirdParty/jquery/js/jquery-ui-1.10.4.custom.min',
-        knockout: '../ThirdParty/knockout/knockout'
+        knockout: '../ThirdParty/knockout/knockout',
+        koExternalTemplateEngine: '../ThirdParty/knockout/koExternalTemplateEngine/koExternalTemplateEngine_all'
     },
 
     shim: {
         'jqueryUI': {
             'exports': '$',
             deps: ['jquery']
+        },
+        'koExternalTemplateEngine': {
+            deps: ['jquery', 'knockout']
         }
     },
 
