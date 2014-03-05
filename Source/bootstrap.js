88f33c343031e6d40cf084d8819ee8835be34747
seanblue <5884067+seanblue@users.noreply.github.com>
2014-03-04 22:09:24 -0500

    Implement save

--- Source/bootstrap.js
+++ Source/bootstrap.js
@@ -29,6 +29,7 @@
         $('#redo-button').button();
         $('#load-button').button();
         $('#save-button').button();
+        $('#save-as-button').button();
         $('#export-button').button();
         $('#refresh-button').button();
         $('#google-analytics-add-button').button();
--- Source/modules/SaveProject.js
+++ Source/modules/SaveProject.js
@@ -20,8 +20,36 @@ define([
             // TODO
         },
 
-        saveProject: function(projectName, viewModel){
-            // TODO
+        saveProject: function(projectSaved, projectName, viewModel){
+            var self = this;
+            var stateObj = viewModel.currentProject.getState();
+
+            $.ajax({
+                type: 'POST',
+                url: 'PHP/saveProject.php',
+                data: {
+                    'project': projectName,
+                    'state': JSON.stringify(stateObj)
+                },
+                success: function(dataString) {
+                    var data = JSON.parse(dataString);
+                    if (data.success) {
+                        // Set the project name in case this is a "Save As"
+                        viewModel.currentProject.name = data.projectName;
+                        viewModel.dirty = false;
+
+                        // TODO: Clear error for Save As popup.
+
+                        projectSaved.resolve();
+                    }
+                    else {
+                        // Display error to user.
+                        // TODO: Display error for Save As popup.
+
+                        projectSaved.reject();
+                    }
+                }
+            });
         }
     };
 
