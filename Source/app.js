c7556ea63f5dcbc22299453573e9b9c59c06ed0f
Kristian Calhoun <kristian.calhoun@gmail.com>
2014-03-05 23:38:53 -0500

    Remove app.js

--- Source/app.js
+++
@@ -1,9 +0,0 @@
-/*global define*/
-/**
- * Wraps the definition of the top-level app module. Loads submodule dependencies.
- */
-define([], function() {
-    'use strict';
-    //Can we remove this?
-    return;
-});
