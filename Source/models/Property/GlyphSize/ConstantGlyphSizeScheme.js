85b7fcdb66dac33bc3be764ef4d5eb1574244c59
Steve Nguyen <st.nguyen09@gmail.com>
2014-03-12 16:23:21 -0400

    Initial glyph size commit

--- Source/models/Property/GlyphSize/ConstantGlyphSizeScheme.js
+++ Source/models/Property/GlyphSize/ConstantGlyphSizeScheme.js
@@ -1,19 +1,66 @@
 /*global define*/
 define([
         'models/Property/GlyphSize/GlyphSizeScheme',
+        'models/Constants/GlyphSizeSchemeType',
+        'models/Property/NumberProperty',
         'knockout',
-        'jquery'
+        'jquery',
+        'util/defined',
+        'util/defaultValue'
     ],function(
         GlyphSizeScheme,
+        GlyphSizeSchemeType,
+        NumberProperty,
         ko,
-        $){
+        $,
+        defined,
+        defaultValue){
     'use strict';
 
     var ConstantGlyphSizeScheme = function(state) {
+        state = defined(state) ? state : {};
+
         GlyphSizeScheme.call(this, state);
 
-        // TODO: Validation, etc
-        this.size = state.size; // NumberProperty
+        if(!defined(state.validValue)){
+            state.validValue = function(value) {
+                return (defined(this.size) && this.size > 0);
+            };
+        }
+
+        var stateSize = {
+            displayName: 'Size',
+            value: 10
+        };
+        if(defined(state.size) && state.size > 0){
+            stateSize.value = state.size;
+        }
+
+        this.size = new NumberProperty(stateSize);
+
+        ko.track(this);
+    };
+
+    ConstantGlyphSizeScheme.prototype = Object.create(GlyphSizeScheme.prototype);
+
+    ConstantGlyphSizeScheme.prototype.getType = function() {
+        return GlyphSizeSchemeType.CONSTANT_SIZE;
+    };
+
+    Object.defineProperties(ConstantGlyphSizeScheme.prototype, {
+        properties: {
+            get: function() {
+                return [this.size];
+            }
+        }
+    });
+
+    ConstantGlyphSizeScheme.prototype.setState = function(state) {
+
+    };
+
+    ConstantGlyphSizeScheme.prototype.getState = function() {
+
     };
 
     return ConstantGlyphSizeScheme;
--- Source/models/Property/GlyphSize/GlyphSizeSelectionProperty.js
+++ Source/models/Property/GlyphSize/GlyphSizeSelectionProperty.js
@@ -3,27 +3,27 @@ define([
         'models/Property/Property',
         'util/defined',
         'models/Constants/PropertyTemplateName',
+        'models/Constants/GlyphSizeSchemeType',
+        'models/Property/GlyphSize/ConstantGlyphSizeScheme',
+        'models/Property/GlyphSize/ScaledGlyphSizeScheme',
         'knockout'
     ],function(
         Property,
         defined,
         PropertyTemplateName,
+        GlyphSizeSchemeType,
+        ConstantGlyphSizeScheme,
+        ScaledGlyphSizeScheme,
         ko
     ){
     'use strict';
 
     var GlyphSizeSelectionProperty = function(state) {
-        Property.call(this, state);
-
-        // TODO: Validation, settings defaults, etc.
-        this._value = state.value; // GlyphSizeScheme
-        this._sizeType = state.sizeType; // ArrayProperty
+        state = defined(state) ? state : {};
+        this.setState(state);
 
         this._templateName = PropertyTemplateName.GLYPH_SIZE;
-
-        this.isValidValue = function(value) {
-            // TODO
-        };
+        this.error = '';
 
         ko.track(this);
     };
@@ -31,18 +31,59 @@ define([
     GlyphSizeSelectionProperty.prototype = Object.create(Property.prototype);
 
     Object.defineProperties(GlyphSizeSelectionProperty.prototype, {
-        sizeType: {
+        value: {
             get: function() {
-                return this._sizeType;
+                return this._value;
             },
             set: function(value) {
-                this._sizeType = value;
+                this._value = value;
+            }
+        },
+        choice: {
+            get: function() {
+                return this._choice;
             }
         }
     });
 
-    GlyphSizeSelectionProperty.prototype.getSelectedSizeType = function() {
-        // TODO
+    // TODO: Uhhhh...this isn't right
+    GlyphSizeSelectionProperty.prototype.getSelectedSizeType = function(schemeType) {
+        if(schemeType === GlyphSizeSchemeType.CONSTANT_SIZE){
+            return new ConstantGlyphSizeScheme();
+        }
+        else if(schemeType === GlyphSizeSchemeType.SCALED_SIZE){
+            return new ScaledGlyphSizeScheme();
+        }
+    };
+
+    GlyphSizeSelectionProperty.prototype.getState = function() {
+        var state = Property.prototype.getState.call(this);
+        return state;
+    };
+
+    GlyphSizeSelectionProperty.prototype.setState = function(state) {
+        Property.call(this, state);
+
+        var constantGlyphSize = new ConstantGlyphSizeScheme(state);
+        var scaledGlyphSize = new ScaledGlyphSizeScheme(state);
+
+        var stateValueType;
+        switch(state.value.type) {
+            case GlyphSizeSchemeType.CONSTANT_SIZE:
+                stateValueType = constantGlyphSize;
+                break;
+            case GlyphSizeSchemeType.SCALED_SIZE:
+                stateValueType = scaledGlyphSize;
+                break;
+            default:
+                stateValueType = undefined; // TODO: get rid of this, testing
+                break;
+        }
+
+        // Always set this to the GlyphSizeSchemeType enums
+        this.sizeType = ko.observableArray([constantGlyphSize, scaledGlyphSize]);
+
+        this._value = ko.observable(stateValueType);
     };
 
     return GlyphSizeSelectionProperty;
