5e51093f2d160c1045fad526d00ef2342bf15f38
seanblue <5884067+seanblue@users.noreply.github.com>
2014-03-15 16:33:00 -0400

    Check extension

--- Source/WAVEDViewModel.js
+++ Source/WAVEDViewModel.js
@@ -106,9 +106,10 @@ define(['jquery',
             displayName: 'File',
             value: '',
             validValue: createValidator({
-                minLength: 1
+                minLength: 1,
+                regex: new RegExp('.(csv|json|xml)$', 'i')
             }),
-            errorMessage: 'Must select a file.'
+            errorMessage: 'Must select a file with extension CSV, JSON, or XML'
         });
 
 
