886980aaf5b59b937a6be1ef649a19b3aaecae27
Keith J. Horrocks <kjh65@cs.drexel.edu>
2014-03-17 18:16:22 -0400

    Single project tree selection
    Instead of having seperate selections within the project tree
    there is now a state of which 'type' is currently selected. The
    delete button at the top now attempts to delete whatever is selected
    (no component deletion at the moment).

--- Source/models/ProjectTree.js
+++ Source/models/ProjectTree.js
@@ -6,9 +6,87 @@ define(['knockout',
     ){
     'use strict';
 
-    var ProjectTree = function(state) {
+    var self;
+    var ProjectTree = function() {
+        self = this;
+        this._selectedType = this.SelectedTypeEnum.PROJECT;
 
+        ko.track(this);
     };
+
+    ProjectTree.prototype.tryToDeleteSelected = function(wavedViewModel) {
+        switch (self._selectedType) {
+            case self.SelectedTypeEnum.PROJECT:
+                return wavedViewModel.tryToDeleteProject();
+            case self.SelectedTypeEnum.COMPONENT:
+                // TODO : Try to delete component
+                return;
+            case self.SelectedTypeEnum.DATA:
+                return wavedViewModel.markDataForDeletion();
+            case self.SelectedTypeEnum.ACTION:
+                return wavedViewModel.removeSelectedAction();
+            case self.SelectedTypeEnum.EVENT:
+                return wavedViewModel.removeSelectedEvent();
+        }
+    };
+
+    ProjectTree.prototype.isSelected = function(wavedViewModel, type, value) {
+        if (self.selectedType != type) {
+            return false;
+        }
+
+        switch (type) {
+            case self.SelectedTypeEnum.PROJECT:
+                return true;
+            case self.SelectedTypeEnum.COMPONENT:
+                return wavedViewModel.selectedWidget === value;
+            case self.SelectedTypeEnum.DATA:
+                return wavedViewModel.selectedDataSet === value;
+            case self.SelectedTypeEnum.ACTION:
+                return wavedViewModel.selectedAction === value;
+            case self.SelectedTypeEnum.EVENT:
+                return wavedViewModel.selectedEvent === value;
+        }
+    };
+
+    ProjectTree.prototype.select = function(wavedViewModel, type, value) {
+        self.selectedType = type;
+
+        switch (type) {
+            case self.SelectedTypeEnum.PROJECT:
+                break;
+            case self.SelectedTypeEnum.COMPONENT:
+                wavedViewModel.selectedWidget = value;
+            case self.SelectedTypeEnum.DATA:
+                wavedViewModel.selectedDataSet = value;
+            case self.SelectedTypeEnum.ACTION:
+                wavedViewModel.selectedAction = value;
+            case self.SelectedTypeEnum.EVENT:
+                wavedViewModel.selectedEvent = value;
+        }
+    };
+
+    Object.defineProperties(ProjectTree.prototype, {
+        SelectedTypeEnum: {
+            get: function() {
+                return {
+                    PROJECT: 0,
+                    COMPONENT: 1,
+                    DATA: 2,
+                    ACTION: 3,
+                    EVENT: 4
+                };
+            }
+        },
+        selectedType: {
+            get: function() {
+                return this._selectedType;
+            },
+            set: function(value) {
+                this._selectedType = value;
+            }
+        }
+    });
 
     return ProjectTree;
 });
