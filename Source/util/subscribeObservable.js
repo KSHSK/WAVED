47295a9316d723a48abc9a800f2593f635e3dbe1
Steve Nguyen <st.nguyen09@gmail.com>
2014-03-20 23:26:54 -0400

    Check if property exists before attempting to subscribe

--- Source/util/subscribeObservable.js
+++ Source/util/subscribeObservable.js
@@ -5,6 +5,10 @@ define(['knockout'], function(ko) {
      * Returns a subscription to a Knockout observable
      */
     var subscribeObservable = function(container, observableName, action, target, event) {
+        if(!(observableName in container)){
+            return null;
+        }
+
         var observable = ko.getObservable(container, observableName);
         if(observable === null){
             return null;
