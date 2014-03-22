54c1f123b9302eedf087748908038997d3a84fbe
seanblue <sean.bluestein@gmail.com>
2014-03-21 20:41:04 -0400

    alt/title

--- Source/WAVEDViewModel.js
+++ Source/WAVEDViewModel.js
@@ -70,9 +70,11 @@ define(['jquery',
         this._projectTree = new ProjectTree();
 
         this._availableWidgets = [{
+            name: 'Button',
             icon: Button.iconLocation(),
             o: Button
         }, {
+            name: 'Text Block',
             icon: TextBlock.iconLocation(),
             o: TextBlock
         }];
