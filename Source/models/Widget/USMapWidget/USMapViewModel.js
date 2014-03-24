1f6daf7f2a577612a617b52f3056df60ff1b4b92
hpinkos <pinkos.hannah@gmail.com>
2014-03-24 18:10:28 -0400

    width/height, multiple maps

--- Source/models/Widget/USMapWidget/USMapViewModel.js
+++ Source/models/Widget/USMapWidget/USMapViewModel.js
@@ -2,45 +2,89 @@ define([
         'models/Event/Trigger',
         'models/Property/Coloring/ColoringSelectionProperty',
         'models/Property/StringProperty',
+        'models/SuperComponentViewModel',
         'models/Widget/WidgetViewModel',
+        'modules/UniqueTracker',
         'util/defined',
         'util/displayMessage',
         'knockout',
         'd3',
-        'jquery',
-        'topojson'
+        'jquery'
     ],function(
         Trigger,
         ColoringSelectionProperty,
         StringProperty,
+        SuperComponentViewModel,
         WidgetViewModel,
+        UniqueTracker,
         defined,
         displayMessage,
         ko,
         d3,
-        $,
-        topojson){
+        $){
     'use strict';
 
-    var self;
     var USMapViewModel = function(state) {
-        self = this;
+        var self = this;
         state = (defined(state)) ? state : {};
         WidgetViewModel.call(this, state);
+
+        if (!defined(state.name)) {
+            var namespace = SuperComponentViewModel.getUniqueNameNamespace();
+            this.name.value = UniqueTracker.getDefaultUniqueValue(namespace, USMapViewModel.getType(), this);
+        }
+
+        this.render = function() {
+            if (self._ready) {
+                var w = $('#waved-workspace').width(); //TODO: this can't use percentages
+                var w2 = w *self.width.value/100;
+                var h = $('#waved-workspace').height();
+                var h2 = h * self.height.value/100;
+                d3.select('#' + self.name.value).selectAll('svg').remove();
+                self._projection = d3.geo.albersUsa().scale(w*1.3*self.width.value/100).translate(([w2/2, h2/2]));
+                var path = d3.geo.path().projection(self._projection);
+                var svg = d3.select('#' + self.name.value)
+                    .append('svg');
+                self._svg = svg;
+                var states = svg.append('g')
+                    .attr('id', 'states');
+                d3.json('data/states.json', function(json) {
+                    states.selectAll('path')
+                        .data(json.features)
+                        .enter()
+                        .append('path')
+                        .attr('d', path)
+                        .attr('stroke', 'white')
+                        .style('filll', function(d) {
+                            return self.coloring.value;
+                        });
+                    self._isRendered = true;
+                    self.updateSvg();
+                });
+            }
+        };
+
+        this.updateSvg = function() {
+            if (self._isRendered) {
+                d3.select('#' + self.name.value).selectAll('svg').selectAll('path')
+                .style('fill', function(d) {
+                    return self.coloring.value;
+                });
+            }
+        };
 
         //TODO: Use ColoringSelectionProperty
         this.coloring = new StringProperty({
             displayName: 'Color',
             value: 'grey',
             errorMessage: 'Must be a color name, hexidecimal number, or rgb color',
-            onchange: function(newValue) {
-                self.updateSvg();
-            }
+            onchange: self.updateSvg
         });
 
+        this.width.onchange = this.render;
+        this.height.onchange = this.render;
         this.height.value = 100;
         this.width.value = 100;
-        this.name.value = 'USMap1';
 
         this.setState(state);
 
@@ -50,6 +94,7 @@ define([
         this._triggers.push(new Trigger({
             name: 'US Map'
         }));
+        this._ready = true;
 
         ko.track(this);
     };
@@ -77,51 +122,6 @@ define([
 
         if (defined(state.coloring)) {
             this.coloring.value = state.coloring.value;
-        }
-    };
-
-    USMapViewModel.prototype.render = function() {
-        var w = $('#waved-workspace').width(); //TODO: this can't use percentages
-        var h = $('#waved-workspace').height();
-        self._projection = d3.geo.albersUsa().translate(([w/2, h/2]));
-        var path = d3.geo.path().projection(self._projection);
-        self._svg = d3.select('#' + self.name.value)
-            .append('svg')
-            .attr('width', w - 20)
-            .attr('height', h - 20);
-        self._g = self._svg.append('g');
-        d3.json('data/states.json', function(error, json) {
-            if (error) {
-                displayMessage('error creating map');
-            } else {
-                self._g.append('g')
-                    .attr('id', 'states')
-                    .selectAll('path')
-                    .data(json.features)
-                    .enter()
-                    .append('path')
-                    .attr('d', path)
-                    .attr('stroke', 'white')
-                    .style('fill', function(d) {
-                        return self.coloring.value;
-                    });
-
-                self._g.append('path')
-                    .datum(topojson.mesh(json, json, function(a, b){ return a !== b; }))
-                    .attr('id', 'state-borders')
-                    .attr('d', path);
-                self._isRendered =  true;
-                self.updateSvg();
-            }
-        });
-    };
-
-    USMapViewModel.prototype.updateSvg = function() {
-        if (self._isRendered) {
-            self._svg.selectAll('path')
-            .style('fill', function(d) {
-                return self.coloring.value;
-            });
         }
     };
 
