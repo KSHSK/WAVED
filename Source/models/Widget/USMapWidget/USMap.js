295e743b13f33def2dc05fbcc65ff7bfbde32804
seanblue <sean.bluestein@gmail.com>
2014-03-31 15:18:40 -0400

    Add map to workspace correctly

--- Source/models/Widget/USMapWidget/USMap.js
+++ Source/models/Widget/USMapWidget/USMap.js
@@ -1,31 +1,35 @@
 define([
         'models/Widget/USMapWidget/USMapViewModel',
         'models/Constants/ComponentTemplateName',
+        '../Widget',
         'knockout',
         'jquery'
     ],function(
         USMapViewModel,
         ComponentTemplateName,
+        Widget,
         ko,
         $){
     'use strict';
 
     var USMap = function(state, getDataSet) {
+        Widget.call(this, state);
+
         this._templateName = ComponentTemplateName.US_MAP;
 
         var viewModel = new USMapViewModel(state, getDataSet);
 
         var $workspace = $('#waved-workspace');
-        var usMap = document.createElement('div');
-        usMap.className = 'widget-container';
-        $(usMap).attr('data-bind', 'template: {name: \'' + this._templateName + '\', ' +
-            'afterRender: render}');
-        $workspace.append(usMap);
+        var usMap = this.newWidgetContainer();
+        usMap.attr('data-bind', 'template: {name: \'' + this._templateName + '\', afterRender: render}');
 
-        this.domElement = usMap;
+        this._domElement = usMap;
         this.viewModel = viewModel;
-        ko.applyBindings(viewModel, usMap);
+
+        ko.applyBindings(viewModel, usMap[0]);
     };
+
+    USMap.prototype = Object.create(Widget.prototype);
 
     /**
      * Static method that returns the type String for this class's ViewModel.
