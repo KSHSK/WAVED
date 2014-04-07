4cd0c8a0153da1ae01e3aa0c0df679d01988ad38
Steve Nguyen <st.nguyen09@gmail.com>
2014-04-07 17:19:43 -0400

    ColoringSelection implementation initial commit

--- Source/models/Property/Coloring/FourColoringScheme.js
+++ Source/models/Property/Coloring/FourColoringScheme.js
@@ -1,21 +1,105 @@
 define([
         'models/Property/Coloring/ColoringScheme',
-        'knockout',
-        'jquery'
+        'models/Property/StringProperty',
+        'models/Constants/ColoringSchemeType',
+        'utils/createValidator',
+        'utils/defined',
+        'knockout'
     ],function(
         ColoringScheme,
-        ko,
-        $){
+        StringProperty,
+        ColoringSchemeType,
+        createValidator,
+        defined,
+        ko){
     'use strict';
 
     var FourColoringScheme = function(state) {
+        state = defined(state) ? state : {};
+
         ColoringScheme.call(this, state);
 
-        // TODO: Define these, validation, etc.
-        this.color1 = state.color1; // StringProperty
-        this.color2 = state.color2; // StringProperty
-        this.color3 = state.color3; // StringProperty
-        this.color4 = state.color4; // StringProperty
+        // All colors default to black
+        this.color1 = new StringProperty({
+            displayName: 'Color #1',
+            value: '#000000',
+            validValue: createValidator({
+                regex: new RegExp(ColoringScheme.prototype.HEX_REGEX)
+            }),
+            errorMessage: ColoringScheme.prototype.INVALID_COLOR_MESSAGE
+        });
+
+        this.color2 = new StringProperty({
+            displayName: 'Color #2',
+            value: '#000000',
+            validValue: createValidator({
+                regex: new RegExp(ColoringScheme.prototype.HEX_REGEX)
+            }),
+            errorMessage: ColoringScheme.prototype.INVALID_COLOR_MESSAGE
+        });
+
+        this.color3 = new StringProperty({
+            displayName: 'Color #3',
+            value: '#000000',
+            validValue: createValidator({
+                regex: new RegExp(ColoringScheme.prototype.HEX_REGEX)
+            }),
+            errorMessage: ColoringScheme.prototype.INVALID_COLOR_MESSAGE
+        });
+
+        this.color4 = new StringProperty({
+            displayName: 'Color #4',
+            value: '#000000',
+            validValue: createValidator({
+                regex: new RegExp(ColoringScheme.prototype.HEX_REGEX)
+            }),
+            errorMessage: ColoringScheme.prototype.INVALID_COLOR_MESSAGE
+        });
+
+        this.setState(state);
+
+        ko.track(this);
+    };
+
+    FourColoringScheme.prototype = Object.create(ColoringScheme.prototype);
+
+    FourColoringScheme.prototype.getType = function() {
+        return ColoringSchemeType.FOUR_COLORING;
+    };
+
+    Object.defineProperties(FourColoringScheme.prototype, {
+        properties: {
+            get: function() {
+                return [this.color1, this.color2, this.color3, this.color4];
+            }
+        }
+    });
+
+    FourColoringScheme.prototype.getState = function() {
+        var state = {
+            color1: this.color1.getState(),
+            color2: this.color2.getState(),
+            color3: this.color3.getState(),
+            color4: this.color4.getState(),
+            type: this.getType()
+        };
+
+        return state;
+    };
+
+    FourColoringScheme.prototype.setState = function(state) {
+        if(defined(state.value.color1)){
+            this.color1.value = state.value.color1.value;
+        }
+        if(defined(state.value.color2)){
+            this.color2.value = state.value.color2.value;
+        }
+        if(defined(state.value.color3)){
+            this.color3.value = state.value.color3.value;
+        }
+        if(defined(state.value.color4)){
+            this.color4.value = state.value.color4.value;
+        }
     };
 
     return FourColoringScheme;
