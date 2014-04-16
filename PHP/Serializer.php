79f8c02a03f18ba4e940111181305892ac88c573
Keith J. Horrocks <KeiferJH@gmail.com>
2014-04-16 19:27:32 -0400

    Excplict writable check for DB

--- PHP/Serializer.php
+++ PHP/Serializer.php
@@ -8,6 +8,11 @@ class Serializer
     private static $_projectSerializer;
 
     private static function initProjectSerializer() {
+        // Only try to connect to the database if it already exists and we can write to it
+        if(!is_writable(self::$DB_PATH)) {
+            throw new Exception("Database does not exist or is not writable");
+        }
+
         $_db = new SQLite3(self::$DB_PATH);
         self::$_projectSerializer = new SQLiteProjectSerializer($_db);
     }
