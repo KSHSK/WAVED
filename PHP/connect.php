3f4d1fd6a54176c8f4f098cc6893781af55cbe66
Steve Nguyen <st.nguyen09@gmail.com>
2014-04-19 13:21:58 -0400

    Continue merge

--- PHP/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once('SQLiteProjectSerializer.php');
-    // Modify this depending on how you are hosting
-    // the website
-    $WEB_ROOT='../DB/';
-    $db = new SQLite3($WEB_ROOT . 'waved.db');
-    $projectSerializer = new SQLiteProjectSerializer($db);
-?>
