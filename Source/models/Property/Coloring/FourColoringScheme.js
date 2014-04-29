d02dd1bb37f99a51a4e179dcce20aeba58680df9
hpinkos <pinkos.hannah@gmail.com>
2014-04-29 00:36:30 -0400

    Update FourColoringScheme.js
    Prettier Colors

--- Source/models/Property/Coloring/FourColoringScheme.js
+++ Source/models/Property/Coloring/FourColoringScheme.js
@@ -22,25 +22,25 @@ define([
         // All colors default to black
         this.color1 = new StringProperty({
             displayName: 'Color #1',
-            value: '#D7FFD0',
+            value: '#69DE83',
             onchange: state.onchange
         });
 
         this.color2 = new StringProperty({
             displayName: 'Color #2',
-            value: '#F2D0FF',
+            value: '#84EAFE',
             onchange: state.onchange
         });
 
         this.color3 = new StringProperty({
             displayName: 'Color #3',
-            value: '#FFFFD0',
+            value: '#FE98E7',
             onchange: state.onchange
         });
 
         this.color4 = new StringProperty({
             displayName: 'Color #4',
-            value: '#FFD0D0',
+            value: '#FEFEAC',
             onchange: state.onchange
         });
 
