e39ce4461d28af7f178bc563c916995a98242cfb
hpinkos <pinkos.hannah@gmail.com>
2014-04-28 23:59:38 -0400

    add SelectType

---
+++ Source/models/Constants/SelectedType.js
@@ -0,0 +1,13 @@
+define([], function(){
+    'use strict';
+
+    var SelectedType = {
+        PROJECT: 0,
+        COMPONENT: 1,
+        DATA: 2,
+        ACTION: 3,
+        EVENT: 4
+    };
+
+    return SelectedType;
+});
