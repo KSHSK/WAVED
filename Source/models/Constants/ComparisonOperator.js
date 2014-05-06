0b13b6afd3cc2f44dd1e17bfcec500b496ad8c25
seanblue <sean.bluestein@gmail.com>
2014-05-06 18:53:23 -0400

    UTF-8 symbols

--- Source/models/Constants/ComparisonOperator.js
+++ Source/models/Constants/ComparisonOperator.js
@@ -2,12 +2,12 @@ define([], function(){
     'use strict';
 
     var ComparisonOperator = {
+        EQUAL: '=',
+        NOT_EQUAL: '≠',
         LESS_THAN: '<',
-        LESS_THAN_OR_EQUAL: '<=',
+        LESS_THAN_OR_EQUAL: '≤',
         GREATER_THAN: '>',
-        GREATER_THAN_OR_EQUAL: '>=',
-        EQUAL: '==',
-        NOT_EQUAL: '!='
+        GREATER_THAN_OR_EQUAL: '≥'
     };
 
     return ComparisonOperator;
