fc60d670ab448c0f64197449b84a9513888784b0
Kristian Calhoun <kristian.calhoun@gmail.com>
2014-05-18 15:02:37 -0400

    Export button CSS intermediate commit.

--- Source/models/Widget/ButtonWidget/Button.js
+++ Source/models/Widget/ButtonWidget/Button.js
@@ -44,5 +44,29 @@ define([
         return 'Source/models/Widget/ButtonWidget/button-icon.png';
     };
 
+    Button.prototype.exportCss = function() {
+
+        // TODO: Add a exportCss function to the widget class. call that to get the standard stuff like
+        // position, display, left, top, width, height, z-index, etc. Then in this function, just concat
+        // the widget-specific css.
+
+        // TODO: id should be widget-id
+        var css = '#' + this.viewModel.name.value + ' {' +
+                  '\tposition: relative;\n' +
+                  '\tdisplay: ' + (this.viewModel.value ? 'block' : 'none') + ';\n' +
+                  '\tleft: ' + this.viewModel.x.originalValue + '%;\n' +
+                  '\ttop: ' + this.viewModel.y.originalValue + '%;\n' +
+                  '\theight: ' + this.viewModel.height.originalValue + '%;\n' +
+                  '\twidth: ' + this.viewModel.width.originalValue + '%;\n' +
+                  '\tz-index: ' + this.viewModel.z.originalValue + ';\n' +
+                  '}';
+
+        return css;
+    };
+
+    Button.prototype.exportHtml = function() {
+        return '<button id="' + this.viewModel.name.value + '">' + this.viewModel.label.originalValue + '</button>';
+    };
+
     return Button;
 });
