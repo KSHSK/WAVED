f474b03dda3ae281f110d60c26ecb8146c1266e6
Kristian Calhoun <kristian.calhoun@gmail.com>
2014-05-18 17:59:36 -0400

    Export - TextBlock

--- Source/models/Widget/TextBlockWidget/TextBlock.js
+++ Source/models/Widget/TextBlockWidget/TextBlock.js
@@ -2,6 +2,7 @@ define([
         'models/Widget/TextBlockWidget/TextBlockViewModel',
         'models/Constants/WidgetTemplateName',
         'models/Event/Trigger',
+        'modules/ExportProject',
         '../Widget',
         'knockout',
         'jquery'
@@ -9,6 +10,7 @@ define([
         TextBlockViewModel,
         WidgetTemplateName,
         Trigger,
+        ExportHelper,
         Widget,
         ko,
         $){
@@ -43,6 +45,27 @@ define([
 
     TextBlock.iconLocation = function() {
         return 'Source/models/Widget/TextBlockWidget/textblock-icon.png';
+    };
+
+    TextBlock.prototype.exportCss = function() {
+        var vm = this.viewModel;
+        var css =  Widget.prototype.exportCss.call(this);
+        css = ExportHelper.addCssProperty(css, 'font-size', vm.textSize.originalValue + 'px');
+        css = ExportHelper.addCssProperty(css, 'text-align', vm.textAlign.originalValue);
+        css = ExportHelper.addCssProperty(css, 'color', vm.textColor.originalValue);
+        css = ExportHelper.addCssProperty(css, 'font-weight', (vm.textWeight.originalValue === true) ? 'bold' : 'normal');
+        css = ExportHelper.addCssProperty(css, 'text-decoration', (vm.textUnderline.originalValue === true) ? 'underline' : 'none');
+        css = ExportHelper.addCssProperty(css, 'background-color', vm.backgroundColor.originalValue);
+        css = ExportHelper.addCssProperty(css, 'border', vm.border.originalValue + 'px solid ' + vm.borderColor.originalValue);
+        css = ExportHelper.addCssProperty(css, 'border-radius', '5px');
+        css = ExportHelper.addCssProperty(css, 'overflow', 'hidden');
+        css = ExportHelper.addCssProperty(css, 'padding', '3px');
+        css = ExportHelper.addCssProperty(css, 'font-family', 'Arial');
+        return css;
+    };
+
+    TextBlock.prototype.exportHtml = function() {
+        return '<div id="' + this.viewModel.name.value + '">' + this.viewModel.text.originalValue + '</div>';
     };
 
     return TextBlock;
