a487c157fc1ae8e52ab5d7e5adb0adf5181c573b
Keith J. Horrocks <kjh65@cs.drexel.edu>
2014-06-05 14:50:20 -0400

    Export start load data (sub)set

--- Source/models/Data/DataSubset.js
+++ Source/models/Data/DataSubset.js
@@ -60,6 +60,13 @@ define([
         }
     };
 
+	DataSubset.prototype.getLoadDataJs = function () {
+		return 'dataSets["' + this.parent.name + '"].dataIsLoaded.then(function () {\n' +
+				'\tdataSets["' + this.name + '"].loadedData = dataSets["' + this.parent.name + '"].loadedData;\n' +
+				'\tdataSets["' + this.name + '"].dataIsLoaded.resolve();\n' +
+				'});\n';
+    };
+
     DataSubset.prototype.executeQuery = function() {
         var self = this;
 
